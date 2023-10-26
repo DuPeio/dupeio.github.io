86dcade136cd19ebc3a09ae7fdf4c0740f9757ed
DuPeio <142165526+DuPeio@users.noreply.github.com>
2023-10-26 17:56:18 +0200

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,19 +1,24 @@
-const btnInfo = document.querySelector(".info");
-const listInfo = document.querySelector(".listInfo");
-const mouseFollow = document.querySelector('.mouseFollow')
-const mouseFollow2 = document.querySelector('.mouseFollow2')
-const mouseFollow3 = document.querySelector('.mouseFollow3')
+function main(){
 
-btnInfo.addEventListener("click", () => {
-    listInfo.classList.toggle("listInfoShow");
-    console.log(listInfo);
-});
+    const btnInfo = document.querySelector(".info");
+    const listInfo = document.querySelector(".listInfo");
+    const mouseFollow = document.querySelector('.mouseFollow')
+    const mouseFollow2 = document.querySelector('.mouseFollow2')
+    const mouseFollow3 = document.querySelector('.mouseFollow3')
+    
+    btnInfo.addEventListener("click", () => {
+        listInfo.classList.toggle("listInfoShow");
+        console.log(listInfo);
+    });
+    
+    window.addEventListener('mousemove', (e) => {
+        mouseFollow.style.left = e.pageX + "px";
+        mouseFollow.style.top = e.pageY + "px";
+        mouseFollow2.style.left = e.pageX + "px";
+        mouseFollow2.style.top = e.pageY + "px";
+        mouseFollow3.style.left = e.pageX + "px";
+        mouseFollow3.style.top = e.pageY + "px";
+    });
+}
 
-window.addEventListener('mousemove', (e) => {
-    mouseFollow.style.left = e.pageX + "px";
-    mouseFollow.style.top = e.pageY + "px";
-    mouseFollow2.style.left = e.pageX + "px";
-    mouseFollow2.style.top = e.pageY + "px";
-    mouseFollow3.style.left = e.pageX + "px";
-    mouseFollow3.style.top = e.pageY + "px";
-});
+window.onload(main);
