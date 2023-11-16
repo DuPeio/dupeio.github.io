2f1e88c0f3f86d94ae50c1fdbd8fb3549d3bbc33
DuPeio <142165526+DuPeio@users.noreply.github.com>
2023-11-16 12:07:32 +0100

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,24 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+function main(){
     const btnInfo = document.querySelector(".info");
     const listInfo = document.querySelector(".listInfo");
     const mouseFollow = document.querySelector('.mouseFollow')
     const mouseFollow2 = document.querySelector('.mouseFollow2')
     const mouseFollow3 = document.querySelector('.mouseFollow3')
-    
+
     btnInfo.addEventListener("click", () => {
         listInfo.classList.toggle("listInfoShow");
         console.log(listInfo);
     });
-    
+
     window.addEventListener('mousemove', (e) => {
+        mouseFollow.style.display = "block";
         mouseFollow.style.left = e.pageX + "px";
         mouseFollow.style.top = e.pageY + "px";
+        mouseFollow2.style.display = "block";
         mouseFollow2.style.left = e.pageX + "px";
         mouseFollow2.style.top = e.pageY + "px";
+        mouseFollow3.style.display = "block";
         mouseFollow3.style.left = e.pageX + "px";
         mouseFollow3.style.top = e.pageY + "px";
     });
-});
+}
+
+window.onload = main;
 
 
